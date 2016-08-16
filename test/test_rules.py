f50d1834580f0b516035b3a5693b45bf4689bed3
Chris Blair <chris.blair36@gmail.com>
2016-08-15 21:53:39 -0500

    Add docstrings to rules unit tests

--- test/test_rules.py
+++ test/test_rules.py
@@ -1,28 +1,37 @@
+""" Tests for the rules module """
+
 import unittest
 from src import rules
 
 class TestRules(unittest.TestCase):
-	def test_good_value_1(self):
-		rules_obj = rules.Rules()
-		result = rules_obj.convertCharToInt('1')
-		self.assertEqual(result, 1)
+    """ Tests for the rules module """
 
-	def test_good_value_3(self):
-		rules_obj = rules.Rules()
-		result = rules_obj.convertCharToInt('3')
-		self.assertEqual(result, 3)
+    def test_good_value_1(self):
+        """ Test a known good value"""
+        rules_obj = rules.Rules()
+        result = rules_obj.convertCharToInt('1')
+        self.assertEqual(result, 1)
 
-	def test_upper_value_10(self):
-		rules_obj = rules.Rules()
-		result = rules_obj.convertCharToInt('10')
-		self.assertEqual(result, 0)
+    def test_good_value_3(self):
+        """ Test a known good value"""
+        rules_obj = rules.Rules()
+        result = rules_obj.convertCharToInt('3')
+        self.assertEqual(result, 3)
 
-	def test_lower_value_5(self):
-		rules_obj = rules.Rules()
-		result = rules_obj.convertCharToInt('-5')
-		self.assertEqual(result, 0)
+    def test_upper_value_10(self):
+        """ Test a value that should be too high"""
+        rules_obj = rules.Rules()
+        result = rules_obj.convertCharToInt('10')
+        self.assertEqual(result, 0)
 
-	def test_bad_value(self):
-		rules_obj = rules.Rules()
-		result = rules_obj.convertCharToInt('qq')
-		self.assertEqual(result, 0)
+    def test_lower_value_5(self):
+        """ Test a value that should be too low"""
+        rules_obj = rules.Rules()
+        result = rules_obj.convertCharToInt('-5')
+        self.assertEqual(result, 0)
+
+    def test_bad_value(self):
+        """ Test a value that isn't an int"""
+        rules_obj = rules.Rules(debug=True)
+        result = rules_obj.convertCharToInt('qq')
+        self.assertEqual(result, 0)
