27aa849d13d9d241eccd7b470e8ccf439b7fdf08
Chris Blair <chris.blair36@gmail.com>
2016-08-15 21:53:17 -0500

    Add module docstring to run_tests

--- test/run_tests.py
+++ test/run_tests.py
@@ -1,7 +1,9 @@
+"""This module uses unittest TestLoader to run tests"""
+
 import sys
 import unittest
 
 if __name__ == '__main__':
-	sys.dont_write_bytecode = True
-	suite = unittest.TestLoader().discover(".")
-	unittest.TextTestRunner(verbosity=2, buffer=True).run(suite)
+    sys.dont_write_bytecode = True
+    suite = unittest.TestLoader().discover(".")
+    unittest.TextTestRunner(verbosity=2, buffer=True).run(suite)
