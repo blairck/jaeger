0068f822dab31eb73ac65740ca8b02975f3342d8
Chris Blair <chris.blair36@gmail.com>
2016-09-11 18:52:51 -0500

    Add class variables and placeholder functions

--- src/historynode.py
+++ src/historynode.py
@@ -1,3 +1,27 @@
 class HistoryNode(GameNode):
     def __init__(self):
+        self.result = None # int
+        self.gameType = None # int
+        self.foxSearch # int
+        self.gooseSearch # int
+        self.halfMove # int
+        self.p1 = None # str
+        self.p2 = None # str
+
+    def print_board(self):
+        pass
+
+    def constructor(self):
+        pass
+
+    def setP1(self, a_string):
+        pass
+
+    def setP2(self, a_string):
+        pass
+
+    def geeseWinP(self):
+        pass
+
+    def foxesWinP(self):
         pass
