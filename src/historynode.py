caaa5162370af0a1bfed42cf08a7c8a35026382e
Chris Blair <chris.blair36@gmail.com>
2016-09-11 22:28:02 -0500

    Add print_board function

--- src/historynode.py
+++ src/historynode.py
@@ -9,7 +9,46 @@ class HistoryNode(GameNode):
         self.p2 = None # str
 
     def print_board(self):
-        pass
+        print("Player 1: {0}".format(self.p1))
+        print("Player 2: {0}".format(self.p2))
+        print("Result: {0}".format(self.result))
+        print("Game Type: {0}".format(self.gameType))
+        print("Fox Search: {0}".format(self.foxSearch))
+        print("Goose Search: {0}".format(self.gooseSearch))
+        print("Half Move: {0}".format(self.halfMove))
+        print("      {0} {1} {2}      ".format(self.gameState[2][6],
+                                               self.gameState[3][6],
+                                               self.gameState[4][6]))
+        print("      {0} {1} {2}      ".format(self.gameState[2][5],
+                                               self.gameState[3][5],
+                                               self.gameState[4][5]))
+        print("{0} {1} {2} {3} {4} {5} {6}".format(self.gameState[0][4],
+                                                   self.gameState[1][4],
+                                                   self.gameState[2][4],
+                                                   self.gameState[3][4],
+                                                   self.gameState[4][4],
+                                                   self.gameState[5][4],
+                                                   self.gameState[6][4]))
+        print("{0} {1} {2} {3} {4} {5} {6}".format(self.gameState[0][3],
+                                                   self.gameState[1][3],
+                                                   self.gameState[2][3],
+                                                   self.gameState[3][3],
+                                                   self.gameState[4][3],
+                                                   self.gameState[5][3],
+                                                   self.gameState[6][3]))
+        print("{0} {1} {2} {3} {4} {5} {6}".format(self.gameState[0][2],
+                                                   self.gameState[1][2],
+                                                   self.gameState[2][2],
+                                                   self.gameState[3][2],
+                                                   self.gameState[4][2],
+                                                   self.gameState[5][2],
+                                                   self.gameState[6][2]))
+        print("      {0} {1} {2}      ".format(self.gameState[2][1],
+                                               self.gameState[3][1],
+                                               self.gameState[4][1]))
+        print("      {0} {1} {2}      ".format(self.gameState[2][0],
+                                               self.gameState[3][0],
+                                               self.gameState[4][0]))
 
     def constructor(self):
         pass
