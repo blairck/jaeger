b341cda7734f81ded2fd2867ec79464af2361889
Chris Blair <chris.blair36@gmail.com>
2016-11-03 21:49:00 -0700

    Replace redundant print statements with print_middle_rows()

--- src/historynode.py
+++ src/historynode.py
@@ -25,27 +25,7 @@ class HistoryNode(gamenode.GameNode):
         print("      {0} {1} {2}      ".format(self.gameState[2][5],
                                                self.gameState[3][5],
                                                self.gameState[4][5]))
-        print("{0} {1} {2} {3} {4} {5} {6}".format(self.gameState[0][4],
-                                                   self.gameState[1][4],
-                                                   self.gameState[2][4],
-                                                   self.gameState[3][4],
-                                                   self.gameState[4][4],
-                                                   self.gameState[5][4],
-                                                   self.gameState[6][4]))
-        print("{0} {1} {2} {3} {4} {5} {6}".format(self.gameState[0][3],
-                                                   self.gameState[1][3],
-                                                   self.gameState[2][3],
-                                                   self.gameState[3][3],
-                                                   self.gameState[4][3],
-                                                   self.gameState[5][3],
-                                                   self.gameState[6][3]))
-        print("{0} {1} {2} {3} {4} {5} {6}".format(self.gameState[0][2],
-                                                   self.gameState[1][2],
-                                                   self.gameState[2][2],
-                                                   self.gameState[3][2],
-                                                   self.gameState[4][2],
-                                                   self.gameState[5][2],
-                                                   self.gameState[6][2]))
+        self.print_middle_rows(" ")
         print("      {0} {1} {2}      ".format(self.gameState[2][1],
                                                self.gameState[3][1],
                                                self.gameState[4][1]))
