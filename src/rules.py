fd3baee8f0a78d298cbc15ec4b91a86ba6bb72d9
Chris Blair <chris.blair36@gmail.com>
2016-12-31 13:01:19 -0800

    Update makeCapture() from C# to Python

--- src/rules.py
+++ src/rules.py
@@ -6,6 +6,13 @@ class Rules(object):
     def __init__(self, debug=False):
         self.debug = debug
 
+    def makeCapture(self, theGame, startX, startY, endX, endY):
+        theGame.setState(startX-1, startY-1, 0)
+        theGame.setState((startX-1)+((endX-1)-(startX-1))/2,
+                         (startY-1)+((endY-1)-(startY-1))/2,
+                         0)
+        theGame.setState(endX-1, endY-1, 2)
+
     def convertCharToInt(self, value):
         """ Converts a string of length 1 (char) to an int. This has an
         intentional return of 0 if the input is non-int. """
