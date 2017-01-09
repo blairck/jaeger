7ee054d3522b43c50a5aa6830b177180829896a3
Chris Blair <chris.blair36@gmail.com>
2017-01-08 20:14:04 -0800

    Remove negative test cases from test_gamenode that are now covered in test_helper

--- test/test_gamenode.py
+++ test/test_gamenode.py
@@ -2,6 +2,7 @@
 
 import unittest
 
+from src import coordinate
 from src import gamenode
 from test import helper
 
@@ -16,30 +17,6 @@ class TestGameNode(unittest.TestCase):
         self.assertFalse(gn_obj.leafP)
         self.assertFalse(gn_obj.rootP)
         self.assertFalse(gn_obj.score)
-
-    def test_getState_default(self):
-        """ Test a known getState value """
-        gn_obj = gamenode.GameNode()
-        result = gn_obj.getState(0, 0)
-        self.assertEqual(result, -1)
-
-    def test_getState_bad_location(self):
-        """ Test getState with a bad location """
-        gn_obj = gamenode.GameNode()
-        self.assertRaises(IndexError, gn_obj.getState, 0, 100)
-
-    def test_setState_good_location(self):
-        """ Test setState with good location """
-        gn_obj = gamenode.GameNode()
-        gn_obj.setState(0, 0, 5)
-        result = gn_obj.getState(0, 0)
-        self.assertEqual(result, 5)
-
-    def test_setState_bad_location(self):
-        """ Test setState with bad location """
-        gn_obj = gamenode.GameNode()
-        self.assertRaises(IndexError, gn_obj.setState, 0, 100, 5)
-
 
     def test_print_board(self):
         """Check that print_board works"""
