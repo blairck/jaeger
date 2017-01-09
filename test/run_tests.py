500da5c8118a0e0fa8fa72bea29942618512a3cb
Chris Blair <chris.blair36@gmail.com>
2017-01-08 20:22:43 -0800

    Run tests with less verbose results

--- test/run_tests.py
+++ test/run_tests.py
@@ -6,4 +6,4 @@ import unittest
 if __name__ == '__main__':
     sys.dont_write_bytecode = True
     suite = unittest.TestLoader().discover(".")
-    unittest.TextTestRunner(verbosity=2, buffer=True).run(suite)
+    unittest.TextTestRunner(verbosity=1, buffer=True).run(suite)
