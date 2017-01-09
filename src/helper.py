33d3f3fcf62807ec5a380d26eb56251f2c0828fd
Chris Blair <chris.blair36@gmail.com>
2017-01-08 19:50:30 -0800

    Add validation method checkIfCoordinateIsValid() and tests

--- src/helper.py
+++ src/helper.py
@@ -5,3 +5,18 @@ def checkIfInt(value):
     if not isinstance(value, int):
         raise TypeError(("value is not an int. "
                          "value = {0}").format(value))
+
+def checkIfCoordinateIsValid(coordinate):
+    """ Check if a coordinate value is a valid location on the board """
+    x = coordinate.get_x_board()
+    y = coordinate.get_y_board()
+    if x < 1 or x > 7:
+        raise ValueError("Invalid board coordinate value, X: ({0}, {1})"
+                         .format(x, y))
+    if y < 1 or y > 7:
+        raise ValueError("Invalid board coordinate value, Y: ({0}, {1})"
+                         .format(x, y))
+    if coordinate.get_x_board() in (1, 2, 6, 7):
+        if coordinate.get_y_board() in (1, 2, 6, 7):
+            raise ValueError("Invalid board coordinate values: ({0}, {1})"
+                             .format(x, y))
--- test/test_helper.py
+++ test/test_helper.py
@@ -1,5 +1,6 @@
 import unittest
 
+from src import coordinate
 from src import helper
 
 class TestHistoryNode(unittest.TestCase):
@@ -10,3 +11,46 @@ class TestHistoryNode(unittest.TestCase):
     def test_checkIfInt_bad(self):
         """ Check that checkIfInt raises an error with non-int input """
         self.assertRaises(TypeError, helper.checkIfInt, "abc")
+
+    def test_checkIfCoordinateIsValid_good(self):
+        """ Check that checkIfCoordinateIsValid doesn't raise exception """
+        testCoordinate = coordinate.Coordinate(3, 4)
+        self.assertFalse(helper.checkIfCoordinateIsValid(testCoordinate))
+
+    def test_checkIfCoordinateIsValid_bad_x(self):
+        """ Check that checkIfCoordinateIsValid does raise an exception """
+        testCoordinate = coordinate.Coordinate(30, 4)
+        self.assertRaises(ValueError,
+                          helper.checkIfCoordinateIsValid,
+                          testCoordinate)
+
+    def test_checkIfCoordinateIsValid_bad_y(self):
+        """ Check that checkIfCoordinateIsValid does raise an exception """
+        testCoordinate = coordinate.Coordinate(3, 40)
+        self.assertRaises(ValueError,
+                          helper.checkIfCoordinateIsValid,
+                          testCoordinate)
+
+    def test_checkIfCoordinateIsValid_bad_x_y(self):
+        """ Check that checkIfCoordinateIsValid does raise an exception """
+        testCoordinate = coordinate.Coordinate(50, 40)
+        self.assertRaises(ValueError,
+                          helper.checkIfCoordinateIsValid,
+                          testCoordinate)
+
+    def test_checkIfCoordinateIsValid_bad_location_x_y(self):
+        """ Check that checkIfCoordinateIsValid does raise an exception """
+        testCoordinate = coordinate.Coordinate(1, 1)
+        self.assertRaises(ValueError,
+                          helper.checkIfCoordinateIsValid,
+                          testCoordinate)
+
+    def test_checkIfCoordinateIsValid_good_location_x(self):
+        """ Check that checkIfCoordinateIsValid does raise an exception """
+        testCoordinate = coordinate.Coordinate(1, 3)
+        self.assertFalse(helper.checkIfCoordinateIsValid(testCoordinate))
+
+    def test_checkIfCoordinateIsValid_good_location_y(self):
+        """ Check that checkIfCoordinateIsValid does raise an exception """
+        testCoordinate = coordinate.Coordinate(6, 4)
+        self.assertFalse(helper.checkIfCoordinateIsValid(testCoordinate))
