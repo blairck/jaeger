5a2c794bebc4b6594eacdaef4409825c135b62d7
Chris Blair <chris.blair36@gmail.com>
2017-04-09 18:23:16 -0700

    Add integration test for isACaptureP()

--- test/test_integ_rules.py
+++ test/test_integ_rules.py
@@ -10,6 +10,19 @@ from src import rules
 
 class TestIntegRules(unittest.TestCase):
     """ Integration tests for the rules module """
+    def test_isACaptureP_direction_4(self):
+        """ IsACaptureP calls direction with a move that doesn't exist """
+        direction = 4
+        rules_obj = rules.Rules()
+        board = gamenode.GameNode()
+        foxCoordinate = coordinate.Coordinate(3, 4)
+        goose_location = coordinate.Coordinate(4, 3)
+        board.setState(foxCoordinate, types.FOX)
+        board.setState(goose_location, types.GOOSE)
+        actual_result = rules_obj.isACaptureP(board, foxCoordinate, direction)
+        expected_result = False
+        self.assertEqual(actual_result, expected_result)
+
     def test_existsCaptureP_capture(self):
         """ End to end test for finding a real goose capture """
         rules_obj = rules.Rules()
