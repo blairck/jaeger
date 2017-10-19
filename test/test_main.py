b5156224fcba23db0ebfcb55b5e5066421badc57
Chris Blair <chris.blair36@gmail.com>
2017-10-18 23:55:00 -0700

    Start adding Main tests

---
+++ test/test_main.py
@@ -0,0 +1,47 @@
+""" Tests for the main module """
+
+import unittest
+from unittest.mock import patch
+
+# pylint: disable=import-error
+from res import types
+from src import ai
+from src import historynode
+from src import coordinate
+import main
+
+class TestMain(unittest.TestCase):
+    """ Tests for the Main module """
+
+    def test_aPlayerHasWon_false(self):
+        """
+        7         S - . - .
+                  | \ | / |
+        6         S - . - .
+                  | / | \ |
+        5 . - . - . - . - . - . - .
+          | \ | / | \ | / | \ | / |
+        4 . - . - . - . - S - . - .
+          | / | \ | / | \ | / | \ |
+        3 . - . - S - S - S - . - .
+                  | \ | / |
+        2         S - S - ~
+                  | / | \ |
+        1         F - F - S
+          1   2   3   4   5   6   7
+        """
+        aiObject = ai.AI()
+        hnObject = historynode.HistoryNode()
+        hnObject.setState(coordinate.Coordinate(3, 1), types.FOX)
+        hnObject.setState(coordinate.Coordinate(4, 1), types.FOX)
+        hnObject.setState(coordinate.Coordinate(5, 1), types.SUPERGOOSE)
+        hnObject.setState(coordinate.Coordinate(3, 2), types.SUPERGOOSE)
+        hnObject.setState(coordinate.Coordinate(4, 2), types.SUPERGOOSE)
+        hnObject.setState(coordinate.Coordinate(5, 4), types.SUPERGOOSE)
+        hnObject.setState(coordinate.Coordinate(3, 3), types.SUPERGOOSE)
+        hnObject.setState(coordinate.Coordinate(4, 3), types.SUPERGOOSE)
+        hnObject.setState(coordinate.Coordinate(5, 3), types.SUPERGOOSE)
+        hnObject.setState(coordinate.Coordinate(3, 6), types.SUPERGOOSE)
+        hnObject.setState(coordinate.Coordinate(3, 7), types.SUPERGOOSE)
+        actualResult = main.aPlayerHasWon(hnObject)
+        self.assertEqual(actualResult, False)
