b9de14efaea1386c1e72f92940ccff11e06408b3
Chris Blair <chris.blair36@gmail.com>
2017-11-11 19:21:08 -0800

    Tweaks to default settings values

--- settings.py
+++ settings.py
@@ -12,10 +12,10 @@ DISPLAYEVALUATION = False
 # is equal to a "half turn," which could either be a goose or fox move.
 # Recommended values: Easy - 1, Medium - 3, Hard - 5, Very Hard - 7
 # Note: Values above 5 take the AI a long time to respond (minutes or more)
-SEARCHPLY = 5
+SEARCHPLY = 3
 
 #  Standard fox positions (True) or random (False).
 STANDARD = True
 
 # User plays Fox (True) or Goose (False).
-USERPLAYSFOX = True
+USERPLAYSFOX = False
